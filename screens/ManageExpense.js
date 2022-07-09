78f2de1445424a6adfca869ec1834f9afc614791
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-07-09 18:06:02 +0700

    06-Update-Delete-BackendData
    06-Update-Delete-BackendData

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,7 +5,7 @@ import ExpenseForm from '../components/ManageExpense/ExpenseForm';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
-import { storeExpense } from '../util/http';
+import { deleteExpense, storeExpense, updateExpense } from '../util/http';
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -24,6 +24,7 @@ function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    deleteExpense(editedExpenseId)
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
@@ -35,6 +36,7 @@ function ManageExpense({ route, navigation }) {
   async function confirmHandler(expenseData) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
+      await updateExpense(editedExpenseId, expenseData);
     } else {
       const id = await storeExpense(expenseData);
       expensesCtx.addExpense({...expenseData, id: id});
